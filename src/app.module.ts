5dcecbdcb56bb00cd89f88e45ed81aa44508f121
Pedro Luis De Leon Alejo <Breidydl@gmail.com>
2024-02-21 22:47:54 -0400

    Que duro

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
+import { User } from './users/entity/users.entity';
 
 @Module({
   imports: [
     UsersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '192.168.0.9',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: 'Users',
-      entities: [__dirname + '//*.entity{.ts,.js}'],
+      entities: [User],
+      // entities: [__dirname + '//*.entity{.ts,.js}'],
       synchronize: true,
     }),
   ],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { UserDto } from './dto/users.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
   createUser(userData: UserDto) {
-    return console.log(userData);
+    return this.userRepository.save(userData);
   }
 }
